db8f05c8c8e5d77ed7c02e8ce2cbacabf4d2b1f0
Tripoli80 <tripoli80@gmail.com>
2022-12-23 07:43:26 +0000

    diet edit

--- src/services/diet.js
+++ src/services/diet.js
@@ -6,7 +6,7 @@ const getDiet = async data => {
     10 * cWeight + 6.25 * height - 5 * age - 161 - 10 * (cWeight - dWeight)
   );
   let key;
-  switch (blood) {
+  switch (blood.toString()) {
     case '1':
       key = { 'groupBloodNotAllowed.1': true };
       break;
@@ -20,19 +20,26 @@ const getDiet = async data => {
       key = { 'groupBloodNotAllowed.4': true };
       break;
     default:
-      console.log(`Sorry, we are out of ${blood}.`);
+      console.log(`Sorry, we are out of ${blood}  ${typeof blood}.`);
   }
 
   // products = aggregat разом з match шукає в базі даних (похоже до find)
   // group = з результату match сортує по категоріям дані (схоже на ruducer)
 
-  const products = await Product.find(key).limit(20);
-  // const products = await Product.aggregate([
-  //   { $match: key },
-  //   {
-  //     $group: { _id: '$categories', count: { $sum: 1 } },
-  //   },
-  // ]);
+  // const products = await Product.find(key).limit(20);
+  if (!key) {
+    return {
+      dailyCalorie,
+      products: [],
+    };
+  }
+
+  const products = await Product.aggregate([
+    { $match: key },
+    {
+      $group: { _id: '$categories', count: { $sum: 1 } },
+    },
+  ]);
   return {
     dailyCalorie,
     products,
