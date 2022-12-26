89054159207fc906ce4cf4d7c28be047049c4a71
Artem <artemgoryuk@gmail.com>
2022-12-26 21:42:57 +0200

    changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 require('dotenv').config();
 const mongoose = require('mongoose');
+
 (() => {
   const PORT = process.env.PORT || 3000;
   const uriDb = process.env.URL_DB;
