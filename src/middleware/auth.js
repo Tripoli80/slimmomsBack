43b03d8a7b2335abef58ae54c7e8380f13ec01ff
Tripoli80 <35936980+Tripoli80@users.noreply.github.com>
2022-12-27 14:58:39 +0000

    Revert "google"

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const { Unauthorized } = require('http-errors');
 const User = require('../models/schemasMongoose/users');
-const secret = process.env.SECRET;
+// const secret = process.env.SECRET;
 
 const auth = async (req, res, next) => {
   const secret = process.env.SECRET;
@@ -16,8 +16,7 @@ const auth = async (req, res, next) => {
     const { _id } = jwt.verify(token, secret);
     const user = await User.findById(_id);
     if (!user) return next(new Unauthorized('Not authorized'));
-    if (user.token != token && user.longtoken != token)
-      return next(new Unauthorized('Not authorized'));
+    if (user.token != token && user.longtoken != token) return next(new Unauthorized('Not authorized'));
 
     req.userId = _id;
     return next();
@@ -26,5 +25,4 @@ const auth = async (req, res, next) => {
     return next(error);
   }
 };
-
 module.exports = auth;
