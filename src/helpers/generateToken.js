d24046b93729d0b0592f2cd4f543bcee13637637
Tripoli80 <tripoli80@gmail.com>
2022-12-29 08:55:53 +0000

    23h

--- src/helpers/generateToken.js
+++ src/helpers/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const generateToken = async _id => {
   const secret = process.env.SECRET;
   const payload = { _id };
-  const token = jwt.sign(payload, secret, { expiresIn: '1m' });
+  const token = jwt.sign(payload, secret, { expiresIn: '23h' });
   const longToken = jwt.sign(payload, secret);
   return { token, longToken };
 };
